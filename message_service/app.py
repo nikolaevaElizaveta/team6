94053727c3d79a988040c80cf70be49f888a39ff
Niyaz <71025204+Vitalo-bit@users.noreply.github.com>
2024-10-10 20:02:22 +0300

    Update app.py
    Added message existance checking

--- message_service/app.py
+++ message_service/app.py
@@ -3,9 +3,10 @@ import requests
 
 app = Flask(__name__)
 
-messages = []  # Список для хранения сообщений
+messages = []  # list for storing messages
 
 USER_SERVICE_URL = 'http://localhost:5001'  # URL User Service
+
 
 @app.route('/post', methods=['POST'])
 def post_message():
@@ -15,18 +16,29 @@ def post_message():
     if len(message) > 400:
         return jsonify({'message': 'Message is too long!'}), 400
 
-    # Проверяем, зарегистрирован ли пользователь
+    # check if user is registered
     response = requests.get(f'{USER_SERVICE_URL}/check', params={'username': username})
     if response.status_code != 200:
         return jsonify({'message': 'User is not registered!'}), 403
 
     messages.append({'username': username, 'message': message})
-    return jsonify({'message': 'Message posted!'}), 201
+    return jsonify({'message': f'Message {len(messages) - 1} posted!'}), 201
+
 
 @app.route('/feed', methods=['GET'])
 def get_feed():
-    # Возвращаем последние 10 сообщений
+    # returning last 10 messages
     return jsonify(messages[-10:]), 200
 
+
+@app.route('/message_check', methods=['GET'])
+def check_message():  # message existence checker, just via try catch on index
+    message = int(request.args.get('message'))
+    try:
+        return jsonify({'Message': f"{messages[message]}"}), 200
+    except IndexError:
+        return jsonify({'Message': 'Message not found'}), 404
+
+
 if __name__ == '__main__':
-    app.run(port=5002)  # Запускаем сервис на порту 5002
+    app.run(port=5002)  # running service on port 5002
